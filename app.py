9d578f55792b68ff3ba7308b409730243b521a77
nabinyang <nabinyang73@gmail.com>
2023-08-12 15:19:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_restx import Api, Resource, fields, Namespace
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import db_config 
+from apis.auth import auth_api
+from apis.board import board_api
+from apis.real_estate import real_estate_api
+from apis.agents import agents_api
+from apis.home_safety_rating import home_safety_rating_api 
+from apis.register_user import register_user_api
+from apis.survey import survey_api
 
 app = Flask(__name__)
+api = Api(
+    app,
+    version='0.1',
+    title="Homey API",
+    description="Homey API Server!",
+    terms_url="/",
+    contact="seungjaelim@kaist.ac.kr",
+    license="MIT"
+)
+api.add_namespace(auth_api)
+api.add_namespace(board_api)
+api.add_namespace(real_estate_api)
+api.add_namespace(agents_api)
+api.add_namespace(home_safety_rating_api)
+api.add_namespace(register_user_api)
+api.add_namespace(survey_api)
 
 @app.route("/")
 def index():
