fb6b89c8da01c8cdfdffe5ce6ed3e60dbf811aa5
Minji Kim <90082373+MJLaboratory@users.noreply.github.com>
2023-08-13 12:34:56 +0900

    Update auth.py

--- templates/auth.py
+++ templates/auth.py
@@ -22,13 +22,13 @@ users = db.users
 @auth_api.route('/kakaoStart', methods=['GET', 'POST'])
 class Starting(Resource):
     def get(self):
-        url = https://kauth.kakao.com/oauth/authorize?client_id=4c682d5d0c62b5c4a5d3e66d9c2c87e0&redirect_uri=https://port-0-flask-ac2nll8rz1xn.sel3.cloudtype.app/oauth&response_type=code
-        #url = 'https://kauth.kakao.com/oauth/authorize'
-        #param = {
-        #    'client_id': '4c682d5d0c62b5c4a5d3e66d9c2c87e0',
-        #    'redirect_uri': 'https://port-0-flask-ac2nll8rz1xn.sel3.cloudtype.app/oauth',
-        #    'response_type': 'code'
-        #    }
+        #url = https://kauth.kakao.com/oauth/authorize?client_id=4c682d5d0c62b5c4a5d3e66d9c2c87e0&redirect_uri=https://port-0-flask-ac2nll8rz1xn.sel3.cloudtype.app/oauth&response_type=code
+        url = 'https://kauth.kakao.com/oauth/authorize'
+        param = {
+            'client_id': '4c682d5d0c62b5c4a5d3e66d9c2c87e0',
+            'redirect_uri': 'https://port-0-flask-ac2nll8rz1xn.sel3.cloudtype.app/oauth',
+            'response_type': 'code'
+            }
         response = requests.request("GET", url, data= json.dumps(param))
 
 @auth_api.route('/oauth', methods=['GET', 'POST'])
