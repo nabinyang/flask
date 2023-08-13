2322dad8a4c6a58a8a06d41c8dfa0912d65f7674
nabinyang <nabinyang73@gmail.com>
2023-08-13 10:09:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask_restx import Api, Resource, fields, Namespace
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 import db_config 
-from apis.auth import auth_api
-from apis.board import board_api
-from apis.real_estate import real_estate_api
-from apis.agents import agents_api
-from apis.home_safety_rating import home_safety_rating_api 
-from apis.register_user import register_user_api
-from apis.survey import survey_api
+from templates.auth import auth_api
+from templates.board import board_api
+from templates.real_estate import real_estate_api
+from templates.agents import agents_api
+from templates.home_safety_rating import home_safety_rating_api 
+from templates.register_user import register_user_api
+from templates.survey import survey_api
 
 app = Flask(__name__)
 api = Api(
@@ -29,15 +29,11 @@ api.add_namespace(home_safety_rating_api)
 api.add_namespace(register_user_api)
 api.add_namespace(survey_api)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
-
-# if __name__ == "__main__":
-#     # Send a ping to confirm a successful connection
-#     try:
-#         # client.admin.command('ping')
-#         print("Pinged your deployment. You successfully connected to MongoDB!")
-#     except Exception as e:
-#         print(e)
-#     app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    # Send a ping to confirm a successful connection
+    try:
+        # client.admin.command('ping')
+        print("Pinged your deployment. You successfully connected to MongoDB!")
+    except Exception as e:
+        print(e)
+    app.run(debug=True, host='0.0.0.0', port=5000)
